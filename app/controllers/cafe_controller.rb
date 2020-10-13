aff73e5aa81f677f01ade9b0169513c67acef509
Nele Taevere <ntaevere@gmail.com>
2020-10-13 14:24:45 +1000

    display data in index.html.erb

--- app/controllers/cafe_controller.rb
+++ app/controllers/cafe_controller.rb
@@ -1,4 +1,12 @@
 class CafeController < ApplicationController
   def index
+    @menu = {
+      latte: 4.00,
+      scone: 5.00,
+      tea: 3.00,
+      donut: 4.00,
+      coffee: 5.00
+    }
   end
+
 end
